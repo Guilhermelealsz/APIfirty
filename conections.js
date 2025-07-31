2b762430afd28cce75f5920aab7d9bfb779d11e4
guilh@Leal <guilhermeleals776@gmail.com>
2025-07-31 13:21:44 -0300

    kk

--- conections.js
+++ conections.js
@@ -3,8 +3,8 @@ import mysql from 'mysql2/promise'
 const conection = await mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  password: 'enzogaeta17',
-  database: 'API'
+  password: 'leal122',
+  database: 'APIdeSOUZA'
 })
 
 export { conection }
